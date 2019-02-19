de25e8acc00d543cc04cba7b9303ecae84ae055b
lccatala <lccm1@alu.ua.es>
2019-02-19 10:38:30 +0100

    Client/Server logic

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 	"os"
 	"strings"
 )
+
+var address = "localhost"
 
 // Error handling
 func checkError(e error) {
@@ -13,17 +17,67 @@ func checkError(e error) {
 	}
 }
 
+// TODO read server configuration from text file
 func readServerConfig() {
 
 }
 
+// Server logic
 func server() {
 	readServerConfig()
+
+	// Listen on port 1337 with tcp protocol
+	line, err := net.Listen("tcp", "localhost:1337")
+	checkError(err)
+	defer line.Close()
 	fmt.Printf("Server is running...\n")
+	for {
+
+		// Accept incomming connections
+		connection, err := line.Accept()
+		checkError(err)
+		connection.Close()
+
+		go func() { // Concurrent lambda function to handle incomming connections
+			// Get remote port
+			_, port, err := net.SplitHostPort(connection.RemoteAddr().String())
+			checkError(err)
+
+			fmt.Print("Connection: ", connection.LocalAddr(), "<-->", connection.RemoteAddr())
+			scanner := bufio.NewScanner(connection)
+
+			// Scan
+			for scanner.Scan() {
+				fmt.Print("client[", port, "]:", scanner.Text()) // Show client message
+				fmt.Fprint(connection, "ack: ", scanner.Text())  // Send ACK to client
+			}
+
+			connection.Close()
+			fmt.Print("close[", port, "]")
+		}()
+	}
 }
 
+// Client logic
 func client() {
 	fmt.Printf("Client is running...\n")
+
+	// Establish connection with server
+	connection, err := net.Dial("tcp", address+":1337")
+	checkError(err)
+	defer connection.Close()
+
+	fmt.Print("Connected to ", connection.RemoteAddr())
+
+	keyscan := bufio.NewScanner(os.Stdin)
+	netscan := bufio.NewScanner(connection)
+
+	// Input scan
+	for keyscan.Scan() {
+		fmt.Fprintln(connection, keyscan.Text()) // Send input to server
+		netscan.Scan()                           // Scan connection
+		fmt.Printf("Server: " + netscan.Text())  // Show server messag
+	}
 }
 
 func main() {
